08b879133ffcf5ad4797b7c951df14067a5c81a5
ydyen <dyen8664@gmail.com>
2020-02-17 10:29:42 +0200

    blog and job app

--- job/views.py
+++ job/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Job
+
+
+def home(request):
+    job = Job.objects.all()
+    return render(request, 'job/job.html', {'jobs': job})
